6c9a565da80baf5106e76462a6bc44ecd4386fc0
posi2436blgyblockerocker <clarencekaiwen@gmail.com>
2023-01-14 02:35:52 +0800

    Update the main.java code

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 public class Main {
     static Scanner scanner = new Scanner(System.in);
-    static Player player = new Player(100, 4.4,"Player");
-    static Player enemyA = new Player(100, 5.5, "Enemy (A.I)");
+    static Player player = new Player(100, 0,"Player");
+    static Player enemyA = new Player(100, 0, "Enemy (A.I)");
     static byte Turn = -1;
     static byte Rounds;
+    static char prefix = '>';
     public static void main(String[] args) {
 
         Turn = 0;
@@ -28,22 +29,34 @@ public class Main {
     {
         System.out.println("====== Round(s) " + Rounds + " ======");
         System.out.println("Options for the " + enemyA.name);
-        System.out.println("1) (A.I) Attack");
-        System.out.println("2) (A.I) Health recharge");
+        System.out.println("1) " + enemyA.name.substring(6, 11) + " Attack");
+        System.out.println("2) " + enemyA.name.substring(6, 11) + " Health recharge\n");
         System.out.println("=== " + enemyA.name + " stats ===\n");
         System.out.println("Health: " + enemyA.health);
-        System.out.println("Damage: " + enemyA.damage);
+        if (Rounds <= 0)
+        {
+            System.out.println("Damage: " + enemyA.damage + " (bonus damage may apply)");
+        }
+        else{
+            System.out.println("Damage: " + enemyA.damage);
+        }
         System.out.println("============");
 
         System.out.println("====== Round(s) " + Rounds + " ======");
         System.out.println("Options for the " + player.name);
-        System.out.println("1) Attack");
+        System.out.println("1) " + player.name + " Attack");
 
         System.out.println("=== " + player.name + " stats ===");
         System.out.println("Health: " + player.health);
-        System.out.println("Damage: " + player.damage);
+        if (Rounds <= 0)
+        {
+            System.out.println("Damage: " + player.damage + " (bonus damage may apply)");
+        }
+        else{
+            System.out.println("Damage: " + player.damage);
+        }
         System.out.println("============");
-        System.out.println(player.name + " turn!");
+        System.out.println(prefix + " " + player.name + " turn!");
     }
     static void options (byte scannerInput)
     {
@@ -51,7 +64,7 @@ public class Main {
             switch (scannerInput) {
                 case 1:
                     if (enemyA.health > 0) {
-                        playerRandomAttackDamageBonus(5, 15);
+                        //playerRandomAttackDamageBonus(0, 0);
 
                         endTurn((byte) 1, player);
 
@@ -69,7 +82,7 @@ public class Main {
     {
         if (enemyA.health > 0) {
 
-            System.out.println("\n " + enemyA.name + " turn!\n");
+            System.out.println("\n" + prefix + " " + enemyA.name + " turn!\n");
             // 0 - 2
             int RandomDecision = ThreadLocalRandom.current().nextInt( 0, 3);
 
@@ -92,37 +105,39 @@ public class Main {
     {
         player.bonusDamage = ThreadLocalRandom.current().nextDouble(minAttackDamageValue, maxAttackDamageValue);
         enemyA.health = (int) (enemyA.health - player.damage - player.bonusDamage);
-        System.out.println(player.name + " did " + player.damage + " base damage \n and " + player.bonusDamage + " bonus damage!");
+        System.out.println(prefix + " " + player.name + " did " + player.damage + " base damage \n and " + player.bonusDamage + " bonus damage!");
     }
     static void enemyRandomAttackDamageBonus(float minAttackDamageValue, float maxAttackDamageValue)
     {
         enemyA.bonusDamage = ThreadLocalRandom.current().nextDouble(minAttackDamageValue, maxAttackDamageValue);
         player.health = (int) (player.health - enemyA.damage - enemyA.bonusDamage);
-        System.out.println(enemyA.name + " did " + enemyA.damage + " base damage \n and " + enemyA.bonusDamage + " bonus damage!");
+        System.out.println(prefix + " " + enemyA.name + " did " + enemyA.damage + " base damage \n and " + enemyA.bonusDamage + " bonus damage!");
 
     }
     static void enemyRandomHealthRegenerate(byte minRegenerateValue, byte maxRegenerateValue)
     {
-        if (enemyA.health < 100) {
             double healthGenerate = ThreadLocalRandom.current().nextDouble(minRegenerateValue, maxRegenerateValue);
             enemyA.health = enemyA.health + (int) healthGenerate;
-            System.out.println(enemyA.name + " healed itself. " + enemyA.name + " now has " + enemyA.health + " health.");
-        }
-        else {
-            System.out.println(enemyA.name + " is already on " + enemyA.health + "/100");
-        }
+            System.out.println(prefix + " " + enemyA.name + " use it's ability to heal itself. \n " + enemyA.name + " now has " + enemyA.health + " health. \n" + "Health boosted to " + (int)healthGenerate);
+
+
     }
     static void enemyRandomDescision(int RandomDecision)
     {
         switch (RandomDecision) {
             case 0:
-                enemyRandomAttackDamageBonus(5, 25);
+                //enemyRandomAttackDamageBonus(0, 0);
                 break;
             case 1:
-                enemyRandomHealthRegenerate((byte) 5, (byte) 15.2);
+                if (enemyA.health > 0 && enemyA.health < 100) {
+
+                    enemyRandomHealthRegenerate((byte) 5, (byte) 15.2);
+                }
+                else {
+                        System.out.println(enemyA.name + " wanted to use it's ability to heal itself but \n is already on " + enemyA.health + "/100\n");
+                    }
                 break;
             case 2:
-                System.out.println("Haven't programmed yet");
                 break;
         }
     }
